40fcf8db13a419b90a1ef2360daa8c29122cabde
Bruno Guimarães <devgmrs@gmail.com>
2026-05-27 03:03:27 -0300

    feat(seo): add sitemap, robots.txt, schema.org, meta tags and favicon

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      lastmod: new Date(),
+      changefreq: 'monthly',
+      priority: 1.0,
+    }),
+  ],
   output: 'static',
   site: 'https://dr-dhiego-ventura.vercel.app',
 });
